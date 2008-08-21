fb96af4cd8399f64e24100e7582ae10fbce62692
Murillo Fernandes Bernardes <mfb@linux.vnet.ibm.com>
2008-08-21 18:00:58 -0300

    Add simple test case for new OvfCertificate.py
    This patch adds a simple test case for the new OvfCertificate.py
    
    Signed-off-by: Murillo Fernandes Bernardes <mfb@linux.vnet.ibm.com>

--- py/tests/OvfCertificateTestCase.py
+++ py/tests/OvfCertificateTestCase.py
@@ -10,12 +10,58 @@
 # Contributors:
 # Eric Casler (IBM) - initial implementation
 ##############################################################################
-import unittest, os
+import unittest
+import os
+import tempfile
+import shutil
+
+from ovf import OvfCertificate
+
+
+CMD_CREATE_CERT = "openssl req -x509 -nodes -days 365 \
+ -subj /C=US/ST=Michigan/L=Plymouth/CN=www.scott.mosers.us \
+ -newkey rsa:512 -keyout %(privkey)s -out %(certificate)s"
 
 TEST_FILES_DIR = os.path.join(os.path.dirname(__file__), "test_files/")
 
 class OvfCertificateTestCase(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.basepath = tempfile.mkdtemp()
+
+        origManifest = os.path.join(TEST_FILES_DIR, 'ourOVF.mf')
+        self.manifest = os.path.join(self.basepath, 'ourOVF.mf')
+        shutil.copyfile(origManifest, self.manifest)
+
+        self.privkey = os.path.join(self.basepath, 'private.pem')
+        self.x509Cert = os.path.join(self.basepath, 'certificate.pem')
+
+        cmd = CMD_CREATE_CERT % {'privkey': self.privkey,
+                                 'certificate': self.x509Cert}
+        OvfCertificate._runCommand(cmd)
+
+        if not os.path.isfile(self.privkey) or \
+                not os.path.isfile(self.x509Cert):
+            raise IOError("Private key or certificate were not created")
+
+    def tearDown(self):
+        shutil.rmtree(self.basepath)
+
+    def test_Sign(self):
+        OvfCertificate.sign(self.manifest, self.privkey, self.x509Cert)
+
+        ovfCert = self.manifest.replace('.mf', '.cert')
+        self.assertTrue(os.path.isfile(ovfCert))
+
+    def test_Verify(self):
+        OvfCertificate.sign(self.manifest, self.privkey, self.x509Cert)
+
+        ovfCert = self.manifest.replace('.mf', '.cert')
+        self.assertTrue(os.path.isfile(ovfCert))
+
+        valid = OvfCertificate.verify(ovfCert)
+        self.assertTrue(valid)
+
 
 if __name__ == "__main__":
     test = unittest.TestLoader().loadTestsFromTestCase(OvfCertificateTestCase)
