0f6d95a1ceaa2183758018a0cc2bb794682ef574
Murillo Fernandes Bernardes <mfb@linux.vnet.ibm.com>
2008-08-21 18:00:16 -0300

    Rewrite OvfCertificate.py
    Now OvfCertificate.py calls the openssl binary directly.
    
    Export functions: sign and verify. Both expect X.509 certificate
    and private key in pem format.
    
    Tested on linux and windows.
    
    Signed-off-by: Murillo Fernandes Bernardes <mfb@linux.vnet.ibm.com>

--- py/ovf/OvfCertificate.py
+++ py/ovf/OvfCertificate.py
@@ -7,171 +7,206 @@
 # http://www.eclipse.org/legal/epl-v10.html
 #
 # Contributors:
-# Scott Moser (IBM) - initial implementation
+# Murillo Fernandes Bernardes (IBM) - initial implementation
 ##############################################################################
+"""OVF Certificate functions.
+
+(This uses openssl directly, not any python bindings to it)
+
+Chek L{sign} and L{verify}.
+"""
+
 import os
+import subprocess
+import binascii
+import tempfile
+import shutil
+import re
 
-#from OpenSSL import crypto
+BASE_CMD = "openssl dgst -sha1 "
 
-class OvfCertificate:
+CMD_SIGN = BASE_CMD + "-hex -sign %(privkey)s -out %(outfile)s %(infile)s"
+CMD_VERIFY = BASE_CMD + "-verify %(pubkey)s -signature %(sign)s %(infile)s"
+
+CMD_GETPUBKEY = "openssl x509 -inform pem -in %(certificate)s -pubkey -noout"
+
+def _runCommand(cmd):
+    """Popen wrapper to simplify the execution of external programs.
+
+    @param cmd: Command line to be executed
+    @type cmd: string
+
+    @return: tuple (exit code, stdout, stderr).
+    @rtype: tuple
     """
-    Object representing a OVF Certificate.  This is used for verification of
-    manifest and for signing the signature of a manifest
 
-    many functions here map to those of U{pyOpenSSL <http://pyopenssl.sourceforge.net/>}
+    ssl = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
+                                            stderr=subprocess.PIPE)
+    status = ssl.wait()
+    out, err = ssl.communicate()
+
+    return status, out, err
+
+def _appendX509Cert(destination, x509Cert):
+    """Append the X.509 certificate to the destination file.
+
+    @param destination: Filename to append the X.509 certificate to
+                        (file should already exist).
+    @type destination: string
+
+    @param x509Cert: X.509 certificate filename
+    @type x509Cert: string
+
+    @raise IOError: If any of the specified files don't exist or
+                    are not readable.
     """
 
-    privateKey = None #: the pkey object from pyopenssl
-    certificate = None #: the X509 object in a .cert file
+    x509CertFile = file(x509Cert, 'r')
+    certFile = file(destination, 'a')
 
-    def __init__(self, path=None):
-        """
-        Initialize OvfCertificate Object
-        """
-        if path != None:
-            getCertificateFromFile(path)
+    certFile.write(x509CertFile.read())
+    x509CertFile.close()
+    certFile.close()
 
-    def generatePrivateKey(self, bits):
-        """
-        Generate private key to be used to sign certificate and digest.
-        """
-        #crypto.PKey().generate_key(crypto.TYPE_RSA, 512)
+def _extractPubkey(x509Cert, destDir=tempfile.gettempdir()):
+    """Extract a public key from a x509 certificate.
 
-    def verify(self, signedKey, manifestFile):
-        """
-        verify the signature of an sha1 digest with the given manifest file
-        This takes file-like object.
+    @param x509Cert: X.509 certificate filename
+    @type x509Cert: string
 
-        @type signedKey    : String
-        @param signedKey   : the signature of an sha1 digest
-        @type manifestFile : String
-        @param manifestFile: path to manifest file
-        """
-        #TODO:
+    @param destDir: Directory to save the pubkey file to.
+    @type destDir: string
 
-    def setCertificate(self, inputString):
-        """
-        Set the certificate. Input can be from a .cert file or a String buffer.
-
-        @type inputString: String
-        @param inputString: path to *.cert file, or String buffer containing certificate
-        """
-        if os.path.isfile(inputString):
-            certificateBuffer = open(inputString, "r").read()
-        else:
-            certificateBuffer = inputString
-        #self.certificate = crypto.load_certificate(crypto.FILETYPE_PEM,certificateBuffer)
-
-    def getCertificate(self):
-        """
-        Return a String buffer representation of the certificate in the X509 object.
-        to a .cert file
-
-        @rtype: String
-        @return: certificate
-        """
-        #return crypto.dump_certificate(crypto.FILETYPE_PEM,self.certificate)
-
-    def loadPrivateKey(self, buf, passphrase):
-        """
-        load a private key, setting the privateKey to be used in signing
-
-        @type buf        : String
-        @param buf       : a String including the private key
-        @type passphrase : String
-        @param passphrase: the passphrase to use for private key, or callback
-
-        @rtype:  boolean
-        @return: success or failure of private key load
-        """
-        #self.privateKey = crypto.load_privatekey(crypto.FILETYPE_PEM,buf,passphrase)
-
-    def signDigest(self, sha1digest):
-        """
-        sign the sha1digest given
-
-        @type sha1digest : String
-        @param sha1digest: a sha1digest to sign
-        @rtype : String
-        @return: a signed digest suitable for writing to .cert file
-            (SHA1(package.mf)= ...)
-        """
-        #TODO:
-
-    def writeCertificate(self, path=None):
-        """
-        Write the certificate to a file, named <basename>.cert
-
-        @type path: String
-        @param path: Save path for file, including filename
-        """
-        if not path.endswith('.cert'):
-            raise ValueError('Type Error: Invalid type, must be of *.cert filetype')
-        else:
-            if os.path.isfile(path) or os.path.isdir(path):
-                certFile = open(path, "w")
-                #certFile.write(crypto.dump_certificate(crypto.FILETYPE_PEM, self.certificate))
-                certFile.close()
-            else:
-                raise IOError('I/O Error: file could not be opened')
-
-def getSignedDigestFromFile(path, passphrase):
+    @return: Public key filename
+    @rtype: string
     """
-    Pull the signed digest out of an OVF .cert file
 
-    @type path: String
-    @param path: path to a .cert file
-    @rtype: String
-    @return: the signed digest from a OVF .cert file
-    """
-    if os.path.isfile(path):
-        if path.endswith('.cert'):
-            certFile = open(path, "r")
-            buf = certFile.readline()
-            while not buf.startswith('SHA1'):
-                buf = certFile.readline()
-            signedDigest = buf.split("=").pop()
-            certFile.close()
-            return signedDigest
-        else:
-            raise TypeError('Type Error: Invalid type, must be of *.cert filetype')
-    else:
-        raise IOError('I/O Error: file could not be opened')
+    cmd = CMD_GETPUBKEY % {'certificate': x509Cert}
+    ret = _runCommand(cmd)
+    if ret[0] != 0:
+        raise
 
-def getCertificateFromFile(path):
-    """
-    Pull the certificate portion of an OVF .cert file
+    pubFd, filename = tempfile.mkstemp(dir=destDir)
+    os.write(pubFd, ret[1])
+    os.close(pubFd)
 
-    @type path : String
-    @param path: path to a .cert file or file handle object for reading
-    @rtype: String
-    @return: the certificate portion of an OVF .cert file
-    """
-    if os.path.isfile(path):
-        if path.endswith('.cert'):
-            certFile = open(path, "r")
-            buf = certFile.readline()
-            while not buf == '-----BEGIN CERTIFICATE-----':
-                buf = certFile.readline()
-            certificateString = buf.append(certFile.read())
-            certFile.close()
-            return certificateString
-        else:
-            raise TypeError('Type Error: Invalid type, must be of *.cert filetype')
-    else:
-        raise IOError('I/O Error: file could not be opened')
+    return filename
 
-def publicKeyFromCertificate(path):
+def _extractSignature(ovfCert, destDir=tempfile.gettempdir()):
+    """Extract the manifest signature from an OVF certificate to
+    a separate binary file, as needed to verify it with openssl.
+
+    @param ovfCert: OVF certificate file
+    @type ovfCert: string
+
+    @param destDir: Directory to save the signature file to.
+    @type destDir: string
+
+    @return: (manifest filename, binary signature filename)
+    @rtype: tuple
     """
-    Given the certificate (as returned by getCertificateFromFile), return the
-    public key
+
+    certFile = file(ovfCert, 'r')
+    line = certFile.readline()
+    certFile.close()
+
+    try:
+        match = re.match('SHA1\((.*)\)= ([A-Za-z0-9]*)', line).groups()
+    except AttributeError:
+        # first line should be SHA1(manifest)= d07fa7bb8a49c114...
+        raise ValueError("ill-formed certificate file.")
+
+    manifest = match[0]
+    digest = match[1]
+
+    binaryDigest = binascii.unhexlify(digest)
+
+    digestFd, filename = tempfile.mkstemp(dir=destDir)
+    os.write(digestFd, binaryDigest)
+    os.close(digestFd)
+
+    return manifest, filename
+
+def sign(manifest, privkey, x509Cert):
+    """Sign a manifest file. The certificate file will have the same
+    basename as the manifest, but with the extension .cert, instead
+    of .mf.
+
+    @param manifest: manifest filename (must end with ".mf").
+    @type manifest: string
+
+    @param privkey: Private key filename
+    @type privkey: string
+
+    @param x509Cert: X.509 certificate filename
+    @type x509Cert: string
+
+    @raise IOError: If any of the specified files don't exist or
+                    are not readable. Or also if the certificate file
+                    can not be created.
     """
-    if os.path.isfile(path):
-        if path.endswith('.cert'):
-            certificateString = getCertificateFromFile(path)
-            #TODO: verify that crypto.dump_privatekey works for public key too.
-            #crypto.dump_privatekey(crypto.FILETYPE_PEM,certificateString.get_pubkey())
-        else:
-            raise TypeError('Type Error: Invalid type, must be of *.cert filetype')
-    else:
-        raise IOError('I/O Error: file could not be opened')
+
+    if not os.path.isfile(manifest):
+        raise IOError("Manifest file not found")
+
+    if not os.path.isfile(privkey):
+        raise IOError("Private key file not found")
+
+    if not os.path.isfile(x509Cert):
+        raise IOError("x509 certificate not found")
+
+    if not manifest.endswith(".mf"):
+        raise ValueError("Manifest file must have .mf extension")
+
+    ovfCert = manifest.replace('.mf', '.cert')
+    manifestDir = os.path.dirname(manifest) # command will run in this dir
+
+    cmd = CMD_SIGN % { 'privkey': privkey,
+                        'outfile': os.path.basename(ovfCert),
+                        'infile': os.path.basename(manifest)}
+
+    origDir = os.getcwd()
+    os.chdir(manifestDir)
+
+    status = _runCommand(cmd)
+    os.chdir(origDir)
+
+    if status[0] != 0:
+        raise ValueError("Openssl failed")
+
+    _appendX509Cert(ovfCert, x509Cert)
+
+def verify(ovfCert):
+    """Verify an OVF signature.
+
+    @param ovfCert: OVF Certificate filename
+    @type ovfCert: string
+
+    @return: True = Valid, False = Not valid
+    @rtype: bool
+
+    @raise IOError: If L{ovfCert}, or the referenced manifest
+                    file don't exist
+    """
+
+    if not os.path.isfile(ovfCert):
+        raise IOError("Certificate file not found")
+
+    tmpdir = tempfile.mkdtemp()
+    pubkey = _extractPubkey(ovfCert, tmpdir)
+    manifest, signature = _extractSignature(ovfCert, tmpdir)
+
+    manifest = os.path.join(os.path.dirname(ovfCert), manifest)
+
+    if not os.path.isfile(manifest):
+        raise IOError("Manifest file not found")
+
+    cmd = CMD_VERIFY % {'pubkey': pubkey, 'sign':  signature,
+                        'infile': manifest}
+
+    status = _runCommand(cmd)
+
+    shutil.rmtree(tmpdir)
+
+    return not status[0] # make it boolean
+
