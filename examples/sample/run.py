a129dd01bbfaef09d7000a6c88c25c409f846ec2
Eric Casler <ejcasler@us.ibm.com>
2008-09-18 10:19:08 -0700

    Added ova to scripts, function --runtime
    ova script in preliminary state, took main and outline from
    
    git://gitorious.org/open-ovf/davel.git -- py/scripts/ova
    
    in order to facilitate the subsequent merge of the rest of that module
    easier later on. Run method was taken from examples/sample/run.py
    
    examples/sample/run.py - modified to use ova
    
    Signed-off-by: Eric Casler <ejcasler@us.ibm.com>
    Signed-off-by: Scott Moser <ssmoser@linux.vnet.ibm.com>

--- examples/sample/run.py
+++ examples/sample/run.py
@@ -12,35 +12,29 @@
 ##############################################################################
 
 from optparse import OptionParser
+import commands
+import os
 
-import libvirt
-
-from ovf import OvfSet
+OVA = "../../py/scripts/ova"
+OVF = "auxiliary/ubuntu-hardy/httpd.ovf"
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("-f", "--file", dest="filename",
-                  help="path to OVF file", metavar="FILE PATH")
     parser.add_option("-c", "--connect", dest="connect",
                   help="libvirt connection URI", metavar="URI")
 
     (options, args) = parser.parse_args()
 
+    OVA_PATH = os.path.join(os.path.dirname(__file__) + "/" + OVA)
+    OVF_PATH = os.path.join(os.path.dirname(__file__) + "/" + OVF)
+
     if options.connect != None:
-        # Open libvirt connection
-        conn = libvirt.open(options.connect)
-
-        if options.filename != None:
-            # Instantiate OvfSet instance for httpd.ovf
-            ovf = OvfSet.OvfSet(options.filename)
-
-            # Boot Virtual Machines
-            ovf.boot(conn)
-
-
-    # Otherwise, just print help/usage message
-    
-        else:
-            parser.print_help()
+        CMD = (OVA_PATH + " --runtime" +
+               " -f " + OVF_PATH +
+               " -c " + str(options.connect))
+        print CMD
+        ret = commands.getstatusoutput(CMD)
+        if ret[0] != 0:
+            print ret[1]
     else:
         parser.print_help()
